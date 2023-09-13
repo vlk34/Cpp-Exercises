ee4bf6f54a8a1ab29cf079335da512e9eba90cd9
Volkan <132361922+vlk34@users.noreply.github.com>
2023-09-13 16:28:26 +0300

    13 Sep
    general stuff in cpp, learned syntax

---
+++ udemy/ctutorial.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cmath>
+#include <ctype.h>
+
+using namespace std;
+
+
+// double cube(int num) {
+//     return num * num * num;
+// }
+
+int main() {
+
+                // STRINGS 
+    /* string phrase ="Giraffe Academy";
+
+    cout << phrase.length() << endl;    // Gives the length of the array
+    cout << phrase.find("Aca", 0) << endl;  // Gives the array position of Aca = 8
+    cout << phrase.substr(8, 3) << endl;    // Grabs 3 characters from [8]
+    return 0; */
+
+                // NUMBERS 
+    /* cout << 40 << endl;
+
+    cout << pow(2, 5) << endl;
+    cout << sqrt(36) << endl;
+
+    cout << round(4.3) << endl; // round
+    cout << ceil(4.3) << endl;  // round up
+    cout << floor(4.3) << endl; // round down
+
+    cout << fmax(3, 10) << endl; // bigger gets returned.
+    cout << fmin(3, 10) << endl; // bigger gets returned. */
+
+                // INPUTTING
+
+    /* int age; 
+    string name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cin.sync();
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    cout << "You are " << age << " years old. ";
+    cout << "Your name is " << name << ". Hello " << name << "!"; */
+
+                // CALCULATOR basic
+
+    /* int num1, num2, sum;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Your sum is " << sum << "."; */
+
+                // MADLIB GAME 
+    // string color, noun, name;
+
+    // cout << "Color: ";
+    // getline(cin, color);
+
+    // cout << "Plural noun: ";
+    // getline(cin, noun);
+
+    // cout << "Name: ";
+    // getline(cin, name);
+
+    // if (!noun.empty()) {
+    //     noun[0] = toupper(noun[0]);
+    // }
+
+    // if (!name.empty()) {
+    //     name[0] = toupper(name[0]);
+    // }
+
+    // cout << "Roses are " << color << endl;
+    // cout << noun << " are blue" << endl;
+    // cout << "I love " << name << endl; 
+
+                // ARRAYS 
+    
+    // int luckyNums[] = {1, 2, 3};
+    // cout << luckyNums[0];
+
+    // int number = 6;
+    // cout << "The answer is: " << cube(number);
+
+                // WHILE LOOP
+
+    // int index = 1;
+    // while (index <= 5) {
+    //     cout << index << endl;
+    //     index++;
+    // }
+
+                // FOR LOOP
+
+    // for (int i = 1; i <= 5; i++) {
+    //     cout << i << endl;
+    // }
+}
